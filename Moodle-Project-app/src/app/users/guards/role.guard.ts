a75417b4fb8bb82541584cf6063c1be8fa834682
AryNG <red.codes.lifes@gmail.com>
2020-01-07 11:50:28 -0600

    WARNING; this code has a commented loop, the error is temporarly fixed, but still on process to be competely corrected, you can only login with an Admin cretendial

--- Moodle-Project-app/src/app/users/guards/role.guard.ts
+++ Moodle-Project-app/src/app/users/guards/role.guard.ts
@@ -8,7 +8,7 @@ import { AuthService } from '../auth/auth.service';
 })
 export class RoleGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) { 
+  constructor(private authService: AuthService, private router: Router) {
     //Authservice is for getting all the information needed to check if the user is admin
     //Router will help to re-direct the user to a certain path if the conditions are meet
   }
@@ -17,7 +17,7 @@ export class RoleGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-     // alert(next.data['role']);
+    // alert(next.data['role']);
     console.log('ROLEGUARD!!!!'); // console confirmation of canActive method 
     if (!this.authService.isLoggedIn()) {
       this.router.navigate(['/login']); //if not logged sent the user to login page
@@ -25,24 +25,25 @@ export class RoleGuard implements CanActivate {
     }
 
     let role = next.data['role'] as string; //catch the 'role' insde the token as a string
-    
-   // console.log('ROLEGUARD->' + next); //Console confirmation of getting the correct role.
+
+    // console.log('ROLEGUARD->' + next); //Console confirmation of getting the correct role.
 
     if (this.authService.hasRole(role)) { //if the date insde the token has a 'role' then
 
-      //Conditional: depending of the type of role will send the user to a specific path.
-      let route; 
+      //Conditional: depending of the type of role will send the user to a specific path. [WARNING]
+      let route;
       if (next.data['role'] == 'ROLE_ADMIN') {
         route = '/Admin';
-      } else if (next.data['role'] == 'ROLE_STUDENT') {
-        route = '/Student';
-      } else if (next.data['role'] == 'ROLE_TEACHER') {
-        route = '/Teacher'
+        /* } else if (next.data['role'] == 'ROLE_STUDENT') {
+           route = '/Student';
+         } else if (next.data['role'] == 'ROLE_TEACHER') {
+           route = '/Teacher'
+         }
+         this.router.navigate([route]);
+       */
       }
-      this.router.navigate([route]);
-    
       return true;
-      
+
     }
   }
 }
