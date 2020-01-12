edf162ede505c8f2b995495824b69ea0b8a26421
AryNG <red.codes.lifes@gmail.com>
2020-01-11 18:47:49 -0600

    You can navigate through the admin, users, create and see a list of all users

--- Gmoodle-app/src/app/MyComponents/Header/header/header.component.ts
+++ Gmoodle-app/src/app/MyComponents/Header/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from 'src/app/MyComponents/Users/functions/auth/auth.service';
+import {Router} from '@angular/router';
+import Swal from 'sweetalert2';
+import { Route } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService : AuthService, private router : Router) { }
+  
+//Method loging with an alert to tell the user that they are logged out!
+  logout():void{
+    let username = this.authService.user.username;
+    this.authService.logout();
+    Swal.fire('Logout', `${username} you have logged out! See you soon.`,'success');
+    this.router.navigate(['/login']); 
+  }
+
 
   ngOnInit() {
   }
