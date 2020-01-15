b56347597bec15119ac6f6efd7c43451e155650d
AryNG <red.codes.lifes@gmail.com>
2020-01-14 18:29:46 -0600

    this one is the good one lol sorry

--- Gmoodle-app/src/app/MyComponents/Dashboards/Admin/admin.component.ts
+++ Gmoodle-app/src/app/MyComponents/Dashboards/Admin/admin.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin.component',
-  templateUrl: './admin.component.component.html',
-  styleUrls: ['./admin.component.component.css']
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+
   }
+  
 
 }
